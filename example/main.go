ebcf2e8be2afb50e89a46a963b48dcd08ef56dbe
Стив <kenethra.74@gmail.com>
2023-05-02 23:05:54 -0600

    doc: update example

--- example/main.go
+++ example/main.go
@@ -7,10 +7,26 @@ import (
 	"github.com/gothew/config"
 )
 
+type ConfigExample struct {
+  Command string `yaml:"command"`
+}
+
+func fnConfigExample() config.ConfigOptions {
+	m := make(map[string]interface{})
+	m["ftp"] = ConfigExample{Command: "ls"}
+
+	return config.ConfigOptions{
+		Services: m,
+	}
+}
+
 func main() {
-	config, err := config.New().ParserConfig()
+	config.SetAppDir("linuxtest")
+	config.SetConfigOptions(fnConfigExample)
+	cfg, err := config.ParseConfig()
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Printf("%v", config)
+
+	fmt.Printf("s1: %s", cfg.Services["ftp"])
 }
